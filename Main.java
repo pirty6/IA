ce91247f9ecd8ca8340076ba478697709e2e01ae
icecreamsoldier <mariana.9000@hotmail.com>
2019-02-25 15:31:37 -0600

    Example for java

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+  public static void main(String[] args) {
+    List<String> lines = new ArrayList<String>();
+    Scanner stdin = new Scanner(System.in);
+    while(stdin.hasNextLine()) {
+      lines.add(stdin.nextLine());
+    }
+    stdin.close();
+
+    int a = Integer.parseInt(lines.get(0));
+    int b = Integer.parseInt(lines.get(1));
+    System.out.println(a+b);
+  }
+}
